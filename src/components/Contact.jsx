3b0fcc03881a815790c4a1b8cac6c88f1bcb2f11
smith-mkb <smithmkb8@gmail.com>
2024-07-20 12:48:37 +0100

    set: size de text sur mobile et desctop

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 0.5 }}
-        className="my-20 text-center text-4xl"
+        className="my-20 text-center text-xl md:text-4xl"
       >
         Entrez en Contact
       </motion.h2>
